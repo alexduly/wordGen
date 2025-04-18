c19b46b5c6c22bad090483f1d32b3e3d9d031370
Alex Duly <alexduly41@gmail.com>
2025-04-18 15:58:21 -0400

    basic working

---
+++ go.mod
@@ -0,0 +1,3 @@
+module shakespeare
+
+go 1.23.8
---
+++ main.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"runtime"
+	"sort"
+	"sync"
+	"time"
+)
+
+const chars = "abcdefghijklmnopqrstuvwxyz"
+
+type result struct {
+	chars string
+	score int
+	count int // testing number of iterations
+}
+
+func routine(ctx context.Context, wg *sync.WaitGroup, cancel context.CancelFunc, ch chan<- result, goal string, id int) {
+	defer wg.Done()
+	fmt.Printf("Running routine %d\n", id)
+	res := result{
+		chars: "",
+		score: 0,
+		count: 0,
+	}
+	for {
+		select {
+		case <-ctx.Done():
+			ch <- res
+			return
+		default:
+			{
+				res.count++
+				seed := rand.New(rand.NewSource(time.Now().UnixNano()))
+				generated := make([]byte, len(goal))
+
+				for i := 0; i < len(goal); i++ {
+					generated[i] = chars[seed.Intn(len(chars))]
+				}
+				tmpChar := ""
+				tmpScore := 0
+
+				for i := 0; i < len(goal); i++ {
+					if goal[i] == generated[i] {
+						tmpChar += string(generated[i])
+						tmpScore++
+					} else {
+						break
+					}
+				}
+				if tmpScore > res.score {
+					res.score = tmpScore
+					res.chars = tmpChar
+				}
+
+				if res.score == len(goal) {
+					deadline, _ := ctx.Deadline()
+					timeleft := time.Until(deadline).Seconds()
+					fmt.Printf("Routine %d has achieved max score, cancelling all and returning. Process finished: %.2fs early\n", id, timeleft)
+					ch <- res
+					cancel()
+
+				}
+			}
+
+		}
+
+	}
+
+}
+
+func main() {
+	maxProcs := runtime.GOMAXPROCS(0) + 2 // Gets max num of cores we can use, add 4 as some will get blocked occasionally available
+	var wg sync.WaitGroup
+	wg.Add(maxProcs)
+	const goal = "shakespeare"
+	ch := make(chan result, maxProcs)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Minute)
+	defer cancel()
+	for i := 0; i < maxProcs; i++ {
+		go routine(ctx, &wg, cancel, ch, goal, i)
+	}
+
+	wg.Wait()
+
+	close(ch)
+	finalCount := 0
+	var results []result
+	for msg := range ch {
+		results = append(results, msg)
+		finalCount += msg.count
+	}
+
+	sort.Slice(results, func(i int, j int) bool {
+		return results[i].score > results[j].score
+	})
+
+	fmt.Printf("Best score achieved was %d with '%s'. A total of %d iterations were made\n", results[0].score, results[0].chars, finalCount)
+}
